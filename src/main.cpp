95a9e0a4cae8a879de30c966ad67e6ec7f25bff3
pedropark99 <pedropark99@gmail.com>
2023-02-06 00:46:15 -0300

    Add more essential files

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <string>
-#include <stdio>
+#include <iostream>
+#include <fstream>
 
 int main (void)
 {
+    std::ifstream bib_file;
+    bib_file.open("./../data/example.bib");
+    if (bib_file.is_open()) {
+        std::string line;
+        while (std::getline(bib_file, line) {
+            std::cout << line << std::endl;
+        }
+        bib_file.close()
+    }
+    
     return 0;
 }
