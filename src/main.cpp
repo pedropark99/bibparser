06caa484831b37c188b829d392d747dfb664389e
pedropark99 <pedropark99@gmail.com>
2023-02-07 23:23:16 -0300

    Add CMAKE configs

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,14 @@
 int main (void)
 {
     std::ifstream bib_file;
-    bib_file.open("./../data/example.bib");
-    if (bib_file.is_open()) {
-        std::string line;
-        while (std::getline(bib_file, line) {
-            std::cout << line << std::endl;
-        }
-        bib_file.close()
+    bib_file.open("./../../../data/example1.bib");
+
+    std::string line;
+    while (std::getline(bib_file, line)) {
+        std::cout << line << std::endl;
     }
+
+    bib_file.close();
     
     return 0;
 }
