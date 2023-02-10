2cde5c9cf6271be50d78b9b3613494070a568853
pedropark99 <pedropark99@gmail.com>
2023-02-09 23:31:23 -0300

    Save last modifications

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,17 @@ int main(void)
 {
 
 
-    std::cout << bibparser::bib_file[1] << std::endl;
-    //bibparser::TraverseString(bibparser::bib_file[1]);
+    // std::cout << bibparser::bib_file[1] << std::endl;
+
+    enum Color { red = 1, green = 2, blue = 3 };
+Color r = red;
+ 
+switch(r)
+{
+    case red  : std::cout << "red\n";   break;
+    case green: std::cout << "green\n"; break;
+    case blue : std::cout << "blue\n";  break;
+}
     
     return 0;
 }
---
+++ src/tokenizer.cpp
@@ -0,0 +1,23 @@
+
+
+enum Token
+{
+    BIB_BEGIN,
+    BIB_IDENTIFIER,
+    BIB_TYPE,
+    BIB_ATTRIBUTE
+};
+
+
+void tokenizer(char chr)
+{
+    switch (chr)
+    {
+    case chr == '@':
+        BIB_BEGIN;
+        break;
+    
+    default:
+        break;
+    }
+}
