4d00373b1819fc9e606dcc29a3255642413b0c9a
pedropark99 <pedropark99@gmail.com>
2023-04-24 10:26:57 -0300

    Add a variable for tracking parser state

--- include/parser.hpp
+++ include/parser.hpp
@@ -2,13 +2,22 @@
 #include <string>
 #include <list>
 #include <unordered_set>
+#include <unordered_map>
 
 #include "tokenizer.hpp"
+
+
+enum ParserState {
+    PARSING = 421,
+    PARSE_ERROR = 422
+};
+
 
 class Parser
 {
 private:
     Tokenizer tokenizer;
+    ParserState parser_state;
     std::unordered_map< TokenType, std::unordered_set<TokenType> >
         expects_next;
     std::list<Token> stack_of_tokens;
